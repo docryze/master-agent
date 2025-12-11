09068f0d36a509dcbebe7213c441a87dad98cb6e
docryze <docryze>
2025-12-12 00:08:28 +0800

    WebSocketDisconnect

--- server/main.py
+++ server/main.py
@@ -1,13 +1,29 @@
-from dotenv import load_dotenv
+from starlette.websockets import WebSocketDisconnect
 from fastapi import FastAPI, WebSocket
-
+from dotenv import load_dotenv
 load_dotenv()
+
 app = FastAPI()
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+
+    try:
+        # 进入通信循环
+        while True:
+            # 尝试接收数据
+            data = await websocket.receive_text()
+
+            # 接收成功，处理数据
+            print(f"Received data: {data}")
+            await websocket.send_text(f"You sent: {data}")
+
+    # 捕获 WebSocketDisconnect 异常
+    except WebSocketDisconnect as e:
+        # 在这里执行清理操作，如从活动连接列表中移除客户端
+        print(f"Client disconnected. Code: {e.code}, Reason: {e.reason}")
+    except Exception as e:
+        # 捕获其他非预期的异常
+        print(f"An unexpected error occurred: {e}")
